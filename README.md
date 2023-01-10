efef9f4ac0f3a8a0d4a87f48d95019f698897449
deepalics0044 <deepalics0044@gmail.com>
2023-01-10 16:39:30 +0530

    update

---
+++ README.md
@@ -0,0 +1,87 @@
+# Photo to 3D (photogrammetry) sample
+
+[![Node.js](https://img.shields.io/badge/Node.js-8.11.1-blue.svg)](https://nodejs.org/)
+[![npm](https://img.shields.io/badge/npm-6.1.0-blue.svg)](https://www.npmjs.com/)
+![Platforms](https://img.shields.io/badge/platform-windows%20%7C%20osx%20%7C%20linux-lightgray.svg)
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://opensource.org/licenses/MIT)
+
+[![oAuth2](https://img.shields.io/badge/oAuth2-v1-green.svg)](http://developer.autodesk.com/)
+[![Reality-Capture](https://img.shields.io/badge/Reality%20Capture-v1-green.svg)](http://developer.autodesk.com/)
+
+# Description
+
+This sample is part of the [Photo to 3D Walkthrough](https://aps.autodesk.com/developer/learn/recap-app).
+
+This sample illustrates the Reality Capture API's photogrammetry workflow:
+
+- Create a photoscene
+- Add images to the photoscene
+- Initiate processing
+- Check for progress
+- Retrieve the generated 3D file
+- Delete the photoscene
+
+## Thumbnail
+
+![thumbnail](/thumbnail.png)
+
+# Setup
+
+## Prerequisites
+
+1. A APS account: [Getting Started with APS](https://aps.autodesk.com/developer/getting-started)
+2. A text editor of your choice. (For example Brackets or Visual Studio Code are good choices.)
+3. A basic knowledge of :
+   - HTML and CSS
+   - JavaScript ES6
+   - Command-line programs
+     - Node.js Command Line (for Windows users)
+     - Terminal (for Mac/Linux/Unix users)
+
+## Running locally
+
+Install [NodeJS](https://nodejs.org/) (version 8 or newer).
+
+Clone this project or download it. It's recommended to install [GitHub desktop](https://desktop.github.com/).
+
+To run it, install the required packages, set the enviroment variables with your client ID & secret and finally start it. Via command line, navigate to the folder where this repository was cloned and use the following:
+
+Mac OSX/Linux (Terminal)
+
+```bash
+npm install
+export APS_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+export APS_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Windows (use Node.js command line from Start menu)
+
+```bash
+npm install
+set APS_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+set APS_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Open a browser and navigate to http://localhost:3000.
+
+Click on the `Authorize me` link and then click through the links on the browser to:
+
+- Add a photoscene
+- Upload files to photoscene
+- Begin processing photoscene
+- Check progress of photoscene (you will see the progress in percentage, keep refreshing until it's DONE)
+- Delete photoscene
+
+_NOTE:_ _It may take several minutes to complete processing a photoscene._
+
+_For a small project (< 20 photos), it shouldn’t take more than 15 minutes to complete, otherwise it takes about 6 hours for 500 photos, and less than 16 hours for 1000 photos. The percentage progress is calibrated for larger projects (that take a few hours to process)._
+
+# License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+# Support
+
+aps.help@autodesk.com
---
+++ start.js
@@ -0,0 +1,334 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+//-------------------------------------------------------------------
+// These packages are included in package.json.
+// Run `npm install` to install them.
+// 'path' is part of Node.js and thus not inside package.json.
+//-------------------------------------------------------------------
+var express = require("express"); // For web server
+var Axios = require("axios"); // A Promised base http client
+var bodyParser = require("body-parser"); // Receive JSON format
+
+// Set up Express web server
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/www"));
+
+// This is for web server to start listening to port 3000
+app.set("port", 3000);
+var server = app.listen(app.get("port"), function () {
+  console.log("Server listening on port " + server.address().port);
+});
+
+//-------------------------------------------------------------------
+// Configuration for your APS account
+// Initialize the 2-legged OAuth2 client, and
+// set specific scopes
+//-------------------------------------------------------------------
+var APS_CLIENT_ID = process.env.APS_CLIENT_ID;
+var APS_CLIENT_SECRET = process.env.APS_CLIENT_SECRET;
+var access_token = "";
+var scopes = "data:read data:write";
+const querystring = require("querystring");
+
+// // Route /api/aps/oauth
+app.get("/api/aps/oauth", function (req, res) {
+  Axios({
+    method: "POST",
+    url: "https://developer.api.autodesk.com/authentication/v1/authenticate",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    data: querystring.stringify({
+      client_id: APS_CLIENT_ID,
+      client_secret: APS_CLIENT_SECRET,
+      grant_type: "client_credentials",
+      scope: scopes,
+    }),
+  })
+    .then(function (response) {
+      // Success
+      access_token = response.data.access_token;
+      console.log(response);
+      res.send(
+        '<p>Authentication success!</p><a href="/api/aps/recap/photoscene/add">Add a photoscene</a>'
+      );
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to authenticate");
+    });
+});
+
+// Route /api/aps/recap/photoscene/add
+// Creates and initializes a photoscene for reconstruction.
+app.get("/api/aps/recap/photoscene/add", function (req, res) {
+  Axios({
+    method: "POST",
+    url: "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+    data: querystring.stringify({
+      scenename: "myscenename",
+      scenetype: "object",
+      format: "rcm",
+    }),
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      var photosceneId = response.data.Photoscene.photosceneid;
+      var nextLink =
+        "/api/aps/recap/photoscene/upload?photosceneid=" + photosceneId;
+      res.send(
+        '<p>Photoscene added!</p><a href="' +
+          nextLink +
+          '">Upload files to photoscene</a>'
+      );
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to create a photoscene");
+    });
+});
+
+// Route /api/aps/recap/photoscene/upload
+// Adds one or more files to a photoscene.
+app.get("/api/aps/recap/photoscene/upload", function (req, res) {
+  var photosceneId = req.query.photosceneid;
+  Axios({
+    method: "POST",
+    url: "https://developer.api.autodesk.com/photo-to-3d/v1/file",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer " + access_token,
+    },
+    data: querystring.stringify({
+      photosceneid: photosceneId,
+      type: "image",
+      "file[0]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1158.JPG",
+      "file[1]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1159.JPG",
+      "file[2]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1160.JPG",
+      "file[3]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1162.JPG",
+      "file[4]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1163.JPG",
+      "file[5]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1164.JPG",
+      "file[6]":
+        "https://s3.amazonaws.com/adsk-recap-public/aps/lion/DSC_1165.JPG",
+    }),
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      console.log(JSON.stringify(response.data.Files));
+      var nextLink =
+        "/api/aps/recap/photoscene/process?photosceneid=" + photosceneId;
+      res.send(
+        '<p>Files added to photoscene!</p><a href="' +
+          nextLink +
+          '">Begin processing photoscene</a>'
+      );
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to upload files to photoscene");
+    });
+});
+
+// Route /api/aps/recap/photoscene/process
+// Starts photoscene processing.
+app.get("/api/aps/recap/photoscene/process", function (req, res) {
+  var photosceneId = req.query.photosceneid;
+  Axios({
+    method: "POST",
+    url:
+      "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/" +
+      photosceneId,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      var nextLink =
+        "/api/aps/recap/photoscene/checkprogress?photosceneid=" + photosceneId;
+      res.send(
+        '<p>Photoscene is being processed!</p><a href="' +
+          nextLink +
+          '">Check progress of photoscene</a>'
+      );
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to process files in photoscene");
+    });
+});
+
+// Route /api/aps/recap/photoscene/checkprogress
+// Returns the processing progress and status of a photoscene.
+app.get("/api/aps/recap/photoscene/checkprogress", function (req, res) {
+  var photosceneId = req.query.photosceneid;
+  Axios({
+    method: "GET",
+    url:
+      "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/" +
+      photosceneId +
+      "/progress",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      if (
+        response.data.Photoscene &&
+        response.data.Photoscene.progressmsg == "DONE"
+      ) {
+        var nextLink =
+          "/api/aps/recap/photoscene/result?photosceneid=" + photosceneId;
+        res.send(
+          '<p>Photoscene process is complete!</p><a href="' +
+            nextLink +
+            '">View result of photoscene</a>'
+        );
+      } else {
+        var nextLink =
+          "/api/aps/recap/photoscene/delete?photosceneid=" + photosceneId;
+        res.send(
+          '<p>Photoscene is not ready, this may take a while. Try refreshing <a href="/api/aps/recap/photoscene/checkprogress?photosceneid=' +
+            photosceneId +
+            '">this page</a>. Progress: ' +
+            response.data.Photoscene.progress +
+            "%...</p>"
+        );
+      }
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to check progress of photoscene");
+    });
+});
+
+// Route /api/aps/recap/photoscene/result
+// Returns a time-limited HTTPS link to an output file of the specified format.
+app.get("/api/aps/recap/photoscene/result", function (req, res) {
+  var photosceneId = req.query.photosceneid;
+  Axios({
+    method: "GET",
+    url:
+      "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/" +
+      photosceneId +
+      "?format=rcm",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      if (
+        response.data.Photoscene &&
+        response.data.Photoscene.progressmsg == "DONE"
+      ) {
+        var nextLink =
+          "/api/aps/recap/photoscene/delete?photosceneid=" + photosceneId;
+        res.send(
+          "<p>Success! This is the scene link:</p><p>" +
+            response.data.Photoscene.scenelink +
+            "</p>" +
+            'Would you like to <a href="' +
+            nextLink +
+            '">delete photoscene</a>?'
+        );
+      } else {
+        res.send(
+          'Photoscene is not ready. Try refreshing <a href="/api/aps/recap/photoscene/checkprogress?photosceneid=' +
+            photosceneId +
+            '">this page</a>. Progress: ' +
+            response.data.Photoscene.progress +
+            "%..."
+        );
+      }
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to get result of photoscene");
+    });
+});
+
+// Route /api/aps/recap/photoscene/delete
+// Deletes a photoscene and its associated assets (images, output files, ...).
+app.get("/api/aps/recap/photoscene/delete", function (req, res) {
+  var photosceneId = req.query.photosceneid;
+  Axios({
+    method: "DELETE",
+    url:
+      "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/" +
+      photosceneId,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      if (response.data.Error) {
+        res.send(response.data.Error.msg);
+      }
+      res.send("<p>Photoscene deleted!</p>");
+    })
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send("Failed to delete photoscene");
+    });
+});
